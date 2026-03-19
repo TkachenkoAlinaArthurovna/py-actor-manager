da0666fa21bf9dd4366f8e383a64dfd19477b9d7
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2026-03-19 12:13:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from managers import ActorManager
-from models import Actor
+from app.managers import ActorManager
+from app.models import Actor
 
 if __name__ == "__main__":
     Actor.objects = ActorManager("actors.sqlite", "actors")
